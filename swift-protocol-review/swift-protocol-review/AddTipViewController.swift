42c3a75b29ddfb110693a52ec7d36703ec57e051
Johann Kerr <johannkerr@gmail.com>
2016-11-08 14:28:54 -0500

    added sample project

---
+++ swift-protocol-review/swift-protocol-review/AddTipViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AddTipViewController.swift
+//  swift-protocol-review
+//
+//  Created by Johann Kerr on 11/8/16.
+//  Copyright © 2016 Johann Kerr. All rights reserved.
+//
+
+import UIKit
+
+class AddTipViewController: UIViewController {
+
+    @IBOutlet weak var tipTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+    @IBAction func addTipBtnPressed(_ sender: Any) {
+        // get tipValueFromTextField 
+        // make sure its a Double
+        //calc tip at 15%
+        // pass back to the first view controller
+    }
+
+   
+}
--- swift-protocol-review/swift-protocol-review/ViewController.swift
+++ swift-protocol-review/swift-protocol-review/ViewController.swift
@@ -8,18 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    
+    
+    var tipArray = [Double]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "basicCell", for: indexPath)
+        
+        
+        return cell
+        
+    }
+    
+    func calcAllTips() -> Double{
+        return 0.0
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
+    @IBAction func calculateAllTips(_ sender: Any) {
+        
+        
+        
+        
+        
+        
+        
+        
+        let alert = UIAlertController(title: "Total Tips", message: "\(calcAllTips())", preferredStyle: .alert)
+  
+        
+        let cancelAction = UIAlertAction(title: "Ok!",
+                                         style: .default)
 
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+    }
 
 }
 
